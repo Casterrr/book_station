e6848f045f5a8a02880ca8c316eeef588618cef2
Filipe Zaidan <felipezaidan10@gmail.com>
2024-07-16 21:46:58 -0300

    fix: update user repository

--- book-station/src/main/java/pooproject/bookstation/repositories/UsuarioRepository.java
+++ book-station/src/main/java/pooproject/bookstation/repositories/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package pooproject.bookstation.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pooproject.bookstation.domain.usuario.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Optional<Usuario> findByEmailUsuario(String emailUsuario);
+
+    boolean existsByEmailUsuario(String emailUsuario);
 }
